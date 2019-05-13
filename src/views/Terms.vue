be4263f2e31011d4d968a5513343398ed319b389
Sota Takahashi <sotq17@gmail.com>
2019-05-13 19:21:45 +0900

    add loading

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -2,12 +2,7 @@
 	<div id ="term">
 		<h1>利用規約</h1>
 		<p>利用規約の文章</p>
-		<router-link :to="{name:'top'}">TOPに戻る</router-link>
+		<router-link :to="{name:'top'}">TOPへ戻る</router-link>
+		<!-- vue-routerでのページ遷移 -->
 	</div>
 </template>
-
-<script>
-export default {
-	name: "terms"
-};
-</script>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,33 +1,43 @@
 <template>
   <div id="app">
+		<div class="loading" v-if="!isLoading"><p>Now Loading...</p>
+			<img src="../assets/gif-load.gif" width="100" height="100">
+		</div>
     <Home v-if="!isLogin"></Home>
+		<!-- ログインしてなければホームを表示 -->
     <Editor v-if ="isLogin" :user="userData"></Editor>
+		<!-- ログインしていればエディター画面を表示 -->
   </div>
 </template>
 
 <script>
 import Home from "../components/Home.vue";
 import Editor from "../components/Editor.vue";
-
+// それぞれのコンポーネントの呼び出し
 
 export default {
   name: 'top',
   data () {
     return {
+			isLoading:false,
       isLogin:false,
-      userData:null
+			userData:null
+			// 初期データはからにしておく
     };
   },
   created: function(){
       firebase.auth().onAuthStateChanged(user =>{
-        console.log(user);
+				console.log(user);
+				this.isLoading = true;
         if (user){
         this.isLogin = true;
-        this.userData = user;
+				this.userData = user;
+				// firebaseにuser情報が入っていればisLoginをtrue
         }
         else{
           this.isLogin = false;
-          rhis.userData = null;
+					this.userData = null;
+					// user情報が入っていなければisLoginをfalse
         };
       });
     },
@@ -36,4 +46,14 @@ export default {
     Editor: Editor
   }
 };
+
 </script>
+
+<style lang="scss">
+.loading {
+  font-family: Nanum Pen Script;
+  font-size: 80px;
+	color:#228B22;
+	padding-top: 10px;
+}
+</style>
